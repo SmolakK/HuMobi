127133be1bdcbb1598734351c5b559f8f06cbd3a
SmolakK <kamilsmolak24@gmail.com>
2022-04-13 08:24:36 +0200

    Docs update

--- generate_docs.bat
+++ generate_docs.bat
@@ -1,3 +1,3 @@
 call C:\ProgramData\Anaconda3\condabin\conda.bat activate HuMobiDeep
-pdoc3 --html D:\\GitHub\\GIT\\HuMobi\\src --output-dir D:\\GitHub\\GIT\\HuMobi\\src\\html --force
+pdoc3 --html D:\\GitHub\\GIT\\HuMobi\\src\\humobi --output-dir D:\\GitHub\\GIT\\HuMobi\\html --force
 pause
